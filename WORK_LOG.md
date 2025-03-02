87da904b71d86ef49dc80d4b199151fbc487f99e
codykletter <cody.kletter@menloschool.org>
2025-03-01 16:55:46 -0800

    3/1 submission

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
-Name: YOUR NAME HERE
+Name: Cody
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date |    Time     |                                                                                                                                                                                                                                Update |
+|:-----|:-----------:|--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| 2/25 | 10:45-11:55 | I was introduced to this problem set and did a lot of thinking about it, specifically first thinking recursively and then moving to a tabulation algorithm. We decided to go from top left corner and fill 2D array all the way down. |
+| 2/27 |  8:45-9:25  |                                                                                                                                 I was able to code the algorithm and it worked! I will now work on finding what the actual string is. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Plagiarism Checker
  * A tool for finding the longest shared substring between two documents.
@@ -15,9 +18,67 @@ public class PlagiarismChecker {
      * @return The length of the longest shared substring.
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
-
-        // TODO Complete this function to return the length of the longest shared substring.
-
-        return 0;
+        return tabulationLSS(doc1, doc2);
+    }
+    public static int tabulationLSS(String doc1, String doc2) {
+        int lenOne = doc1.length();
+        int lenTwo = doc2.length();
+        // Create array for tabulation (first row and column already filled with 0s)
+        int[][] shared = new int[lenOne + 1][lenTwo + 1];
+        // Loop through each index in array
+        for (int i = 1; i <= lenOne; i++) {
+            for (int j = 1; j <= lenTwo; j++) {
+                // If the letters match between letters at the given index,
+                // current index becomes index of top left square above it plus one
+                if (doc1.charAt(i - 1) == doc2.charAt(j - 1)) {
+                    shared[i][j] = shared[i - 1][j - 1] + 1;
+                }
+                // If no match, current index becomes the max of the left and right squares
+                else {
+                    shared[i][j] = Math.max(shared[i - 1][j], shared[i][j - 1]);
+                }
+            }
+        }
+        ArrayList<String> longestSubstrings = new ArrayList<String>();
+        boolean[][] visited = new boolean[lenOne + 1][lenTwo + 1];
+        findLongest(longestSubstrings, shared, lenOne, lenTwo, "", doc1, doc2, shared[lenOne][lenTwo], visited);
+        for (String longest : longestSubstrings) {
+            System.out.println(longest);
+        }
+        // Return bottom right corner
+        return shared[lenOne][lenTwo];
+    }
+    public static void findLongest(ArrayList<String> longestSubstrings, int[][] shared, int i, int j, String current, String doc1, String doc2, int longestLength, boolean[][] visited) {
+        int difference = longestLength - current.length();
+        if (visited[i][j]) {
+            return;
+        }
+        if (i - difference < 0 || j - difference < 0) {
+            return;
+        }
+        if (i == 0 || j == 0) {
+            if (current.length() == longestLength) {
+                longestSubstrings.add(current);
+            }
+            return;
+        }
+        // Mark square as visited
+        visited[i][j] = true;
+        if (shared[i - 1][j - 1] == shared[i][j] - 1 && doc1.charAt(i - 1) == doc2.charAt(j - 1)) {
+            current = doc1.charAt(i - 1) + current;
+            findLongest(longestSubstrings, shared, i - 1, j - 1, current, doc1, doc2, longestLength, visited);
+        }
+        else {
+            if (shared[i - 1][j] == shared[i][j - 1]) {
+                findLongest(longestSubstrings, shared, i - 1, j, current, doc1, doc2, longestLength, visited);
+                findLongest(longestSubstrings, shared, i, j - 1, current, doc1, doc2, longestLength, visited);
+            }
+            else if (shared[i - 1][j] > shared[i][j - 1]) {
+                findLongest(longestSubstrings, shared, i - 1, j, current, doc1, doc2, longestLength, visited);
+            }
+            else {
+                findLongest(longestSubstrings, shared, i, j - 1, current, doc1, doc2, longestLength, visited);
+            }
+        }
     }
 }
